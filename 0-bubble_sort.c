102b7bfb1261c87e439d0402c9108eb39af327c7
Derhks <derhks@gmail.com>
2020-02-01 01:29:25 +0000

    [updated]

---
+++ 0-bubble_sort.c
@@ -0,0 +1,22 @@
+#include "sort.h"
+
+
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t itr = 0, co = 0;
+	int tmp = 0;
+	for (co = 0; co < size; co++)
+	{
+		for (itr = 0; itr < size; itr++)
+		{
+			if (array[itr] > array[itr + 1] && array[itr+1])
+			{
+				tmp = array[itr];
+				array[itr] = array[itr + 1];
+				array[itr + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+}
--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,20 +8,27 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t **tmp = NULL, **tmp_prv = NULL **tmp_nxt = NULL;
+	listint_t *tmp = NULL, *tmp_prv = NULL, *tmp_nxt = NULL;
 
 	if (*list)
 	{
-		*tmp = *list;
+		tmp = *list;
 		while (tmp->next)
 		{
 			if (tmp->n > tmp->next->n)
 			{
 				if (tmp->prev)
 				{
-				        
+					tmp->next = tmp_nxt;
+					tmp_nxt->prev->prev = tmp_prv;
+					tmp_nxt->prev->next = tmp;
+					tmp->prev = tmp->next->prev;
+					tmp_nxt->prev = tmp;
+					tmp_prv->next = tmp->prev;
 				}
+				print_list(*list);
 			}
+			tmp = tmp->next;
 		}
 	}
 }
