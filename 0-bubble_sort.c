3a67c7771d44b55e686502f583122a7f3a0a25eb
Cris-Mur <yomy98@gmail.com>
2020-02-03 17:07:35 +0000

    [betty's recomendations]

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,16 +1,21 @@
 #include "sort.h"
 
-
-
+/**
+ * bubble_sort - bubble sort algorithm
+ * @array: input unsorted array
+ * @size: size of the array
+ * Return: void
+ */
 void bubble_sort(int *array, size_t size)
 {
 	size_t itr = 0, co = 0;
 	int tmp = 0;
+
 	for (co = 0; co < size; co++)
 	{
 		for (itr = 0; itr < size; itr++)
 		{
-			if (array[itr] > array[itr + 1] && array[itr+1])
+			if (array[itr] > array[itr + 1] && array[itr + 1])
 			{
 				tmp = array[itr];
 				array[itr] = array[itr + 1];
