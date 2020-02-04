d383fac5bb46314fa24deeeceba4ed5db086653e
Cris-Mur <yomy98@gmail.com>
2020-02-04 18:47:47 +0000

    [fixed redundant conditional && add big o task 2 and 3]

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -12,50 +12,46 @@ void insert_end(listint_t *tmp, listint_t *tmp_aux, listint_t *tmp_nxt);
 void insertion_sort_list(listint_t **list)
 {
 	int sort = 0;
-	listint_t *tmp = NULL, *tmp_aux = NULL, *tmp_nxt = NULL;
+	listint_t *tmp = *list, *tmp_aux = NULL, *tmp_nxt = NULL;
 
-	if (*list)
+	while (*list && tmp->next)
 	{
-		tmp = *list;
-		while (tmp->next)
+		if (tmp->n > tmp->next->n)
 		{
-			if (tmp->n > tmp->next->n)
+			if (tmp->next->next)
 			{
-				if (tmp->next->next)
+				tmp_aux = tmp->next;
+				tmp_nxt = tmp->next->next;
+				tmp->next = tmp_nxt;
+				tmp_nxt->prev = tmp;
+				if (!tmp->prev)
 				{
-					tmp_aux = tmp->next;
-					tmp_nxt = tmp->next->next;
-					tmp->next = tmp_nxt;
-					tmp_nxt->prev = tmp;
-					if (!tmp->prev)
-					{
-						insert_beg(tmp_aux, tmp);
-						*list = tmp_aux;
-					}
-					else
-					{
-						insert_mid
-							(tmp, tmp_nxt, tmp_aux);
-					}
-					sort = 1;
+					insert_beg(tmp_aux, tmp);
+					*list = tmp_aux;
 				}
-				else if (!tmp->next->next)
+				else
 				{
-					insert_end(tmp, tmp_aux, tmp_nxt);
-					sort = 1;
-					}
-				print_list(*list);
+					insert_mid(tmp, tmp_nxt, tmp_aux);
+				}
+				sort = 1;
 			}
-			if (sort)
+			else if (!tmp->next->next)
 			{
-				tmp = *list;
-				sort = 0;
+				insert_end(tmp, tmp_aux, tmp_nxt);
+				sort = 1;
 			}
-			else
-				tmp = tmp->next;
+			print_list(*list);
 		}
+		if (sort)
+		{
+			tmp = *list;
+			sort = 0;
+		}
+		else
+			tmp = tmp->next;
 	}
 }
+
 
 /**
  * insert_beg - insert aux node at begining of the list
